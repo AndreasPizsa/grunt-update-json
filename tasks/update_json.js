73db3c009815000261938ff45a8e28e056f8307a
Nicholas Bollweg (Nick) <nick.bollweg@gmail.com>
2014-04-19 15:30:15 -0400

    curious state after promoting to git

--- tasks/update_json.js
+++ tasks/update_json.js
@@ -6,55 +6,98 @@
  * Licensed under the MIT license.
  */
 
-'use strict';
 
 module.exports = function(grunt) {
-
+  'use strict';
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
 
-  grunt.registerMultiTask('update_json','Update JSON files with data from other JSON files',function(){
-    var _=require('lodash');
-    var self = this;
+  var _ = require('lodash'),
+    pointer = require('json-pointer');
 
-    this.files.forEach(function(file){
-      grunt.verbose.debug(file);
-      var theSourceData = {};
+  grunt.registerMultiTask(
+    'update_json',
+    'Update JSON files with data from other JSON files',
+    function(){
+      var task = this,
+        debug = grunt.verbose.debug,
+        warn = grunt.fail.warn,
+        exists = grunt.file.exists,
+        json = grunt.file.readJSON,
+        option = grunt.option;
+      
+      option.init(grunt.config.data.update_json.options || {});
+      
+      task.files.forEach(function(file){
+        debug(file);
+        var fields = task.data.fields,
+          output = exists(file.dest) ? json(file.dest) : {},
+          input = file.src.filter(function(src) {
+              grunt.verbose.debug(src);
+              if(grunt.file.exists(src)){ return true; }
+              warn('File ' + src + ' not found.');
+            }).reduce(function(data, src){
+              debug(src);
+              return _.merge(data, grunt.file.readJSON(src));
+            }, {}),
+          copied = {};
 
-      file.src.filter(function(filepath) {
-        grunt.verbose.debug(filepath);
-        var theFileExists = grunt.file.exists(filepath);
-        if(!theFileExists) grunt.fail.warn('File ' + filepath + ' not found.');
-        return theFileExists;
-      }).map(function(filepath){
-        grunt.verbose.debug(filepath);
-        var theFileData = grunt.file.readJSON(filepath);
-        theSourceData = _.merge(theSourceData,theFileData);
+        // put fields in canonical (key-value object) form
+        // first, break up a single string, if found
+        fields = !_.isString(fields) ? fields : fields.split(/\s*,\s*/);
+
+        // then, turn an array of fieldspecs:
+        //  ["field", "from>to", {"from": "to"}]
+        fields = !_.isArray(fields) ? fields : _.reduce(fields, mergeField, {});
+
+        copied = _.reduce(fields, expandField(input), copied);
+
+        if(output == null) {
+          warn('Could not read file ' + file.dest);
+        }
+
+        grunt.file.write(
+          file.dest,
+          JSON.stringify( _.merge(output, copied), null, option('indent'))
+        );
       });
+    }
+  );
+  
+  // factory for a reduce function, bound to the input, that can get get
+  // the value out of 
+  function expandField(input){
+    return function(memo, fin, fout){
+      memo[fout] =
+        // field name, interpret as an absolute JSON pointer
+        _.isString(fin) ? pointer.get(input, "/" + fin) :
+        // call a function
+        _.isFunction(fin) ? fin(input) :
+        // pick out the values
+        _.isArray(fin) ? _.values(_.pick(input, fin)) :
+        // build up an object of something else
+        _.isObject(fin) ? _.reduce(fin, expandField(input), {}) :
+        // copy the value
+        _.isNull(fin) ? input[fout] :
+        warn('Could not map ' + fin + ' to ' + fout);
 
-      if(!_.isPlainObject(self.data.fields)) {
-        theSourceData = _.pick(theSourceData,self.data.fields);
-      }
-      else {
-        theSourceData = _.pick(theSourceData,_.keys(self.data.fields));
-        _.forEach(self.data.fields, function(value,key){
-          grunt.log.debug(value,key);
-          if(value && theSourceData.hasOwnProperty(key)) {
-            theSourceData[value] = theSourceData[key];
-            delete theSourceData[key];
-          }
-        });
-      }
+      return memo;
+    };
+  }
+  
+  // Parse a fieldspec, like ["field", "to<from", {"to": "from"}]
+  function mergeField(memo, key){
+    var result = {},
+      match;
 
-      var theDest = grunt.file.exists(file.dest) 
-            ? grunt.file.readJSON(file.dest)
-            : {};
-      if(theDest == null) {
-        grunt.fail.warn('Could not read file ' + file.dest);
-      }
+    if(_.isPlainObject(key)){
+      result = key;
+    }else if(_.isString(key) && (match = key.match(/^(.*?)\s*>\s*(.*?)$/))){
+      result[match[2]] = match[1];
+    }else{
+      result[key] = null;
+    }
 
-      theDest = _.merge(theDest,theSourceData);
-      grunt.file.write(file.dest, JSON.stringify(theDest,null,'\t') + '\n');
-    });
-  })
+    return _.merge(memo, result);
+  }
 };
